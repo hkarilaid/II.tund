7b3ac2e15254a40d27af27f8d958a574ad2dbe6c
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2016-02-10 11:13:34 +0200

    functions

--- app.js
+++ app.js
@@ -8,7 +8,28 @@
     }
     Moosipurk.instance = this;
 
-      console.log('moosipurgi sees');
+      console.log(this);
+
+      this.click_count = 0;
+      this.init();
+  };
+  //K6ik funktsioonid tulevad siia sisse
+  Moosipurk.prototype = {
+    init: function(){
+      console.log('rakendus käivitus');
+      //siia tuleb esialgne loogika
+      //hakka kuulama hiireklõpse
+      this.bindMouseEvents();
+    },
+    bindMouseEvents: function(){
+      document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+
+    },
+    addNewClick: function(event){
+      //console.log(event);
+      this.click_count++;
+      console.log(this.click_count);
+    }
   };
 
   window.onload = function(){
